1794fb3985351a6081479804a0fc486c29936683
howlee <112477130@qq.com>
2018-02-24 12:35:44 +0800

    代码提交
    播放页面 微信打开 增加 是否本站 引用判断。

--- public/js/public/pc/player.js
+++ public/js/public/pc/player.js
@@ -34,7 +34,7 @@ function isWeiXin() {
     }
 }
 function LoadVideo () {
-    if (isWeiXin()) {
+    if (isWeiXin() && parent.location.href.indexOf('aikq.cc') != -1) {
         //在这里写如果是微信的时候的状态
         $('#MyFrame').html('<p class="noframe">请使用 <b>浏览器</b> 打开<img class="code" src="/img/pc/code.jpg">加微信 <b>fs188fs</b><br/>与球迷赛事交流，乐享高清精彩赛事！</p>')
         return;
