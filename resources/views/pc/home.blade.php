e7a953b9d2dd7b50d7a49bb66bbf288126709135
howlee <112477130@qq.com>
2018-03-29 10:10:42 +0800

    刷新首页样式。

--- resources/views/pc/home.blade.php
+++ resources/views/pc/home.blade.php
@@ -1,6 +1,6 @@
 @extends('pc.layout.base')
 @section('css')
-    <link rel="stylesheet" type="text/css" href="{{env('CDN_URL')}}/css/pc/home.css?time=20180203030001">
+    <link rel="stylesheet" type="text/css" href="{{env('CDN_URL')}}/css/pc/home.css?time=20180203030002">
 @endsection
 @section('content')
     <div id="Content">
