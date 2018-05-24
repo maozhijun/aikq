14467a220eeafee935c2043e4b19883a8b894829
xieweijie <BJ@192.168.2.103>
2018-05-24 11:00:59 +0800

    json位置调整

--- app/Http/Controllers/PC/FIFA/WorldCupController.php
+++ app/Http/Controllers/PC/FIFA/WorldCupController.php
@@ -44,7 +44,7 @@ class WorldCupController extends Controller{
         //重点的比赛
         $json = self::curlData('https://www.liaogou168.com/aik/worldcup/hotMatch',5);
         if (isset($json)){
-            Storage::disk("public")->put("/static/worldcup/2018/hotmatch.json", json_encode($json));
+            Storage::disk("public")->put("/json/worldcup/2018/hotmatch.json", json_encode($json));
         }
         $rest['top']['focus_matches'] = $json;
         //焦点图
