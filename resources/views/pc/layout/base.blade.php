8cb2be68067316bc96d67af21d11a5495b2f9469
howlee <112477130@qq.com>
2018-07-31 11:25:59 +0800

    修改关键词

--- resources/views/pc/layout/base.blade.php
+++ resources/views/pc/layout/base.blade.php
@@ -23,7 +23,7 @@
     </script>
     <?php
         $title = isset($title) ? $title : '爱看球-爱看球直播|JRS直播|NBA直播|英超直播|西甲直播|低调看|直播吧|免费直播';
-        $keywords = isset($keywords) ? $keywords : '爱看球,爱看球直播,JRS直播,NBA直播,英超直播,西甲直播,足球直播,篮球直播,低调看,直播吧,免费直播';
+        $keywords = isset($keywords) ? $keywords : '爱看球,爱看球直播,JRS直播,NBA直播吧,英超直播,西甲直播,足球直播,篮球直播,低调看直播,免费直播';
         $description = isset($description) ? $description : '爱看球是一个专业为球迷提供免费的NBA,CBA,英超,西甲,德甲,意甲,法甲,中超,欧冠,世界杯等各大体育赛事直播、解说平台，无广告，无插件，高清，直播线路多';
     ?>
     <meta charset="UTF-8">
@@ -47,7 +47,6 @@
         <a class="column{{isset($check) && $check == 'videos' ? ' on' : ''}}" href="/live/subject/videos/all/1.html">录像</a>
         <a class="column" href="https://www.liaogou168.com/recommends.html" target="_blank">推荐</a>
         <a class="column {{isset($check) && $check == 'business' ? ' on' : ''}}" href="/live/business.html" target="_blank">源调用</a>
-        {{--<a class="column{{isset($check) && $check == 'basket' ? ' on' : ''}}" href="/basketball.html">篮球</a>--}}
         <a class="column" href="/download.html" target="">下载</a>
         @yield('nav_inner')
     </div>
