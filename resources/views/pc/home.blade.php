a4f0a62aa3456ed19db502b6cacd1b52cc5d29b2
xieweijie <BJ@192.168.2.105>
2018-08-15 15:56:13 +0800

    akq css js缓存时间调整

--- resources/views/pc/home.blade.php
+++ resources/views/pc/home.blade.php
@@ -1,6 +1,6 @@
 @extends('pc.layout.base')
 @section('css')
-    <link rel="stylesheet" type="text/css" href="{{env('CDN_URL')}}/css/pc/home.css?time=20180203030007">
+    <link rel="stylesheet" type="text/css" href="{{env('CDN_URL')}}/css/pc/home.css?time=201808151553">
 @endsection
 @section('content')
     <div id="Content">
@@ -101,7 +101,7 @@
     {{--</div>--}}
 @endsection
 @section('js')
-    <script type="text/javascript" src="{{env('CDN_URL')}}/js/public/pc/home.js?time=2018012619310002"></script>
+    <script type="text/javascript" src="{{env('CDN_URL')}}/js/public/pc/home.js?time=201808151555"></script>
     <script type="text/javascript">
         window.onload = function () { //需要添加的监控放在这里
             setADClose();
