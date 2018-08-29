1764a716b38d009b288764387960362ab8893658
xieweijie <BJ@192.168.2.112>
2018-08-29 18:31:32 +0800

    pc 增加mobile agent

--- resources/views/pc/anchor/index.blade.php
+++ resources/views/pc/anchor/index.blade.php
@@ -1,4 +1,4 @@
-@extends('pc.layout.anchor_base')
+@extends('pc.layout.base')
 @section('css')
     <link rel="stylesheet" type="text/css" href="{{env('CDN_URL')}}/css/pc/anchor.css?time=201807251702">
     <style>
