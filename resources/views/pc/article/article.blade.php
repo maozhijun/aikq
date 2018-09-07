09f1d6a7081b137d2609dee610d196cff4daa316
howlee <112477130@qq.com>
2018-09-07 17:05:24 +0800

    按湿胸要求，修改 文章终端样式。

--- resources/views/pc/article/article.blade.php
+++ resources/views/pc/article/article.blade.php
@@ -2,7 +2,6 @@
 @section("css")
     <link rel="stylesheet" type="text/css" href="{{env('CDN_URL')}}/css/pc/article.css?rd={{date('YmdHi')}}">
     <link rel="stylesheet" type="text/css" href="{{env('CDN_URL')}}/css/pc/jqcloud.css?rd={{date('YmdHi')}}">
-
 @endsection
 @section("content")
     <div id="Content">
@@ -67,7 +66,6 @@
                         @foreach($res as $re)
                             <a target="_blank" href="{{$re->url}}">{{$re->title}}</a>
                         @endforeach
-                        <p class="clear"></p>
                     </div>
                 @endif
             </div>
