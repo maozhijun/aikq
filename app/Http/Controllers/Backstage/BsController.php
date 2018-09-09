40f8e4aad63ff79b3f3b2f45e0a888fd47d2aff9
ricky <843372983@qq.com>
2018-09-09 16:46:22 +0800

    主播后台 修改获取推流地址的响应时间为5s

--- app/Http/Controllers/Backstage/BsController.php
+++ app/Http/Controllers/Backstage/BsController.php
@@ -629,7 +629,7 @@ class BsController extends Controller
     {
         $host = env('PUSH_URL', 'http://live.push.qiushengke.com');
         $url = $host . '/api/v1/get_push_stream?uid=' . $room_id . '&level=' . $level . ($refresh ? '&refresh=1' : '');
-        $jsonStr = \App\Http\Controllers\Controller::execUrl($url, 2, $isHttp);
+        $jsonStr = \App\Http\Controllers\Controller::execUrl($url, 5, $isHttp);
         $json = json_decode($jsonStr, true);
         return $json;
     }
