0240a09c67e942064072f7d078d3ba69c5d70fc5
howlee <112477130@qq.com>
2018-10-08 15:58:24 +0800

    修正json路径。

--- public/js/public/pc/anchor_player.js
+++ public/js/public/pc/anchor_player.js
@@ -9,7 +9,7 @@ var active_code = '';//'/img/pc/code.jpg';//'/img/pc/i_wx_code.jpg';
 var valid_code = '8888', show_ad = true, matchTime, matchStatus, errorRepeat = 1;
 
 $.ajax({
-    "url": "/m/dd_image/images.json?time=" + (new Date()).getTime(),
+    "url": "/json/dd_image/images.json?time=" + (new Date()).getTime(),
     "success": function (json) {
         if (json) {
             if (json.l) ad_l = json.l;
