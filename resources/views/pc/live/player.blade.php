39f1e8b45f51673b4804968b81e370845f37507b
ricky <843372983@qq.com>
2018-10-31 16:14:51 +0800

    ……

--- resources/views/pc/live/player.blade.php
+++ resources/views/pc/live/player.blade.php
@@ -7,13 +7,13 @@
     <title>爱看球直播</title>
     {{--<meta name="Keywords" content="JRS,JRS直播,NBA直播,NBA录像,CBA直播,英超直播,西甲直播,足球直播,篮球直播,低调看,直播吧,CCTV5在线,CCTV5+">--}}
     {{--<meta name="Description" content="爱看球是一个专业为球迷提供免费的NBA,CBA,英超,西甲,德甲,意甲,法甲,中超,欧冠,世界杯等各大体育赛事直播、解说平台，无广告，无插件，高清，直播线路多">--}}
-    <link rel="stylesheet" type="text/css" href="{{env('CND_URL')}}/css/pc/style.css?rd=2018">
-    <link rel="stylesheet" type="text/css" href="{{env('CND_URL')}}/css/pc/player.css?rd=201808201154">
+    <link rel="stylesheet" type="text/css" href="{{env('CDN_URL')}}/css/pc/style.css?rd=2018">
+    <link rel="stylesheet" type="text/css" href="{{env('CDN_URL')}}/css/pc/player.css?rd=201808201154">
     <meta http-equiv="X-UA-Compatible" content="edge" />
     <meta name="renderer" content="webkit|ie-stand|ie-comp">
     <meta name="baidu-site-verification" content="nEdUlBWvbw">
     <meta name="viewport" content="width=device-width, initial-scale=0.5, maximum-scale=0.5, minimum-scale=0.5, user-scalable=no">
-    <link rel="Shortcut Icon" data-ng-href="{{env('CND_URL')}}/img/pc/ico.ico" href="{{env('CND_URL')}}/img/pc/ico.ico">
+    <link rel="Shortcut Icon" data-ng-href="{{env('CDN_URL')}}/img/pc/ico.ico" href="{{env('CDN_URL')}}/img/pc/ico.ico">
 </head>
 <body scroll="no">
 <div class="player_content" id="MyFrame">
@@ -25,10 +25,10 @@
 <script type="text/javascript" src="{{env('CDN_URL')}}/js/public/pc/jquery_191.js"></script>
 <![endif]-->
 <script type="text/javascript" src="//imgcache.qq.com/open/qcloud/video/vcplayer/TcPlayer-2.2.0.js"></script>
-<script type="text/javascript" src="{{$cdn}}/js/public/pc/ckplayer/ckplayer.js?timd=2018030300007"></script>
+<script type="text/javascript" src="{{env('CDN_URL')}}/js/public/pc/ckplayer/ckplayer.js?timd=2018030300007"></script>
 
 <script type="text/javascript">
-    window.jsonHost = '{{env("API_HOST")}}';
+    window.jsonHost = '{{env("API_URL")}}';
     function isMobileWithJS() {
         var u = navigator.userAgent;
         var isAndroid = u.indexOf('Android') > -1; //android终端或者uc浏览器
@@ -55,8 +55,8 @@
     }
     //window.CKHead = (location.href.indexOf('https://') != -1 ? 'https:' : 'http:') + '{{$cdn}}/js/public/pc/ckplayer/';
 </script>
-<script src="{{env('CND_URL')}}/js/public/pc/socket.io.js"></script>
-<script type="text/javascript" src="{{env('CND_URL')}}/js/public/pc/player3.js?rd=201809061137"></script>
+<script src="{{env('CDN_URL')}}/js/public/pc/socket.io.js"></script>
+<script type="text/javascript" src="{{env('CDN_URL')}}/js/public/pc/player3.js?rd=201809061137"></script>
 <script>
     $.ajaxSetup({
         headers: {'X-CSRF-TOKEN': '{{ csrf_token() }}'}
@@ -89,5 +89,5 @@
     }
     showDownload();
 </script>
-{{--<script type="text/javascript" src="{{env('CND_URL')}}/js/testSocket3.js?timd={{date('YmdHi')}}"></script>--}}
+{{--<script type="text/javascript" src="{{env('CDN_URL')}}/js/testSocket3.js?timd={{date('YmdHi')}}"></script>--}}
 </html>
