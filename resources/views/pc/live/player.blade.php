0f97103c92a27c3e9c3b57fedd48b5aafa2f7ec6
howlee <112477130@qq.com>
2018-11-07 17:46:44 +0800

    取消no-referrer标签

--- resources/views/pc/live/player.blade.php
+++ resources/views/pc/live/player.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="robots"content="nofollow">
-    <meta name="referrer" content="no-referrer">
+    {{--<meta name="referrer" content="no-referrer">--}}
     <title>爱看球直播</title>
     {{--<meta name="Keywords" content="JRS,JRS直播,NBA直播,NBA录像,CBA直播,英超直播,西甲直播,足球直播,篮球直播,低调看,直播吧,CCTV5在线,CCTV5+">--}}
     {{--<meta name="Description" content="爱看球是一个专业为球迷提供免费的NBA,CBA,英超,西甲,德甲,意甲,法甲,中超,欧冠,世界杯等各大体育赛事直播、解说平台，无广告，无插件，高清，直播线路多">--}}
