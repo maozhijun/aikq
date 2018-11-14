611755cc29568cda6a957de403a6643b947d44bb
xieweijie <BJ@xieweijiedeMacBook-Pro.local>
2018-11-14 16:34:15 +0800

    js文件

--- app/Http/Controllers/PC/Live/LiveController.php
+++ app/Http/Controllers/PC/Live/LiveController.php
@@ -876,6 +876,7 @@ class LiveController extends Controller
             $pc_json = json_encode($jsonData);
             if (!empty($pc_json)) {
                 Storage::disk("public")->put("static/json/pc/match/live/url/channel/". $id . '.json', $pc_json);
+                Storage::disk("public")->put("static/json/pc/match/live/url/channel/". $id . '.js','var a = '. json_encode($pc_json));
                 //每一个channel的player页面生成
                 $json = json_decode($pc_json,true);
                 if (strlen($player) > 0 && $json && array_key_exists('code',$json) && $json['code'] == 0) {
@@ -910,10 +911,12 @@ class LiveController extends Controller
                 $mobile_json = $pc_json;
                 if (!empty($mobile_json)) {
                     Storage::disk("public")->put("static/json/m/match/live/url/channel/". $id . '.json', $mobile_json);
+                    Storage::disk("public")->put("static/json/m/match/live/url/channel/". $id . '.js', 'var a = '.json_encode($mobile_json));
                 }
             } else {
                 if (!empty($pc_json)) {
                     Storage::disk("public")->put("static/json/m/match/live/url/channel/". $id . '.json', $pc_json);
+                    Storage::disk("public")->put("static/json/m/match/live/url/channel/". $id . '.js', 'var a = '.json_encode($pc_json));
                 }
             }
         } catch (\Exception $e) {
@@ -939,6 +942,7 @@ class LiveController extends Controller
             $pc_json = json_encode($jsonStr);
             if (!empty($pc_json)) {
                 Storage::disk("public")->put("static/json/pc/match/live/url/channel/". $id . '.json', $pc_json);
+                Storage::disk("public")->put("static/json/pc/match/live/url/channel/". $id . '.js','var a = '. json_encode($pc_json));
                 //每一个channel的player页面生成
                 $json = json_decode($pc_json,true);
                 if (strlen($player) > 0 && $json && array_key_exists('code',$json) && $json['code'] == 0) {
@@ -970,10 +974,12 @@ class LiveController extends Controller
                 $mobile_json = $pc_json;
                 if (!empty($mobile_json)) {
                     Storage::disk("public")->put("static/json/m/match/live/url/channel/". $id . '.json', $mobile_json);
+                    Storage::disk("public")->put("static/json/m/match/live/url/channel/". $id . '.js', 'var a = '.json_encode($mobile_json));
                 }
             } else {
                 if (!empty($pc_json)) {
                     Storage::disk("public")->put("static/json/m/match/live/url/channel/". $id . '.json', $pc_json);
+                    Storage::disk("public")->put("static/json/m/match/live/url/channel/". $id . '.js', 'var a = '.json_encode($pc_json));
                 }
             }
         } catch (\Exception $e) {
