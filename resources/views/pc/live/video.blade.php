b3621a40a9eb38e00194e1261a76f46505799dd2
howlee <112477130@qq.com>
2019-01-25 17:56:25 +0800

    优化pc终端页

--- resources/views/pc/live/video.blade.php
+++ resources/views/pc/live/video.blade.php
@@ -102,13 +102,14 @@
                     {{--<span>直播线路：</span>--}}
                     <?php $channels = $live['channels']; ?>
                     @if(isset($channels))
+                        <?php $btnIndex = 0; ?>
                         @foreach($channels as $index=>$channel)
                             @continue($channel['platform'] == \App\Models\Match\MatchLiveChannel::kPlatformApp) {{-- $channel['player'] == \App\Models\Match\MatchLiveChannel::kPlayerExLink ||  --}}
                             <?php
                                 if ($channel['player'] == \App\Models\Match\MatchLiveChannel::kPlayerExLink) {
                                     $url = $channel["link"];
                                 } else {
-                                    $url = env('LHB_URL').'/room/' . $match["sport"] . $match["mid"] . '.html';
+                                    $url = env('LHB_URL').'/room/' . $match["sport"] . $match["mid"] . '.html?btn=' . ($btnIndex++);
                                 }
                             ?>
                             <a href="{{$url}}" target="_blank">{{$channel['name']}}</a>
