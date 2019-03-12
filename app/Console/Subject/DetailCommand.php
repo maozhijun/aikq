f38c89ef5572b0b2e03e4d0849b8f818c97ce755
howlee <112477130@qq.com>
2019-03-12 18:44:53 +0800

    修改定时任务

--- app/Console/Subject/DetailCommand.php
+++ app/Console/Subject/DetailCommand.php
@@ -9,6 +9,7 @@
 namespace App\Console\Subject;
 
 
+use App\Console\HtmlStaticCommand\BaseCommand;
 use App\Http\Controllers\IntF\AikanQController;
 use App\Http\Controllers\PC\Live\SubjectController;
 use App\Models\Subject\SubjectLeague;
@@ -16,30 +17,19 @@ use Illuminate\Console\Command;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
 
-class DetailCommand extends Command
+class DetailCommand extends BaseCommand
 {
-    /**
-     * The name and signature of the console command.
-     *
-     * @var string
-     */
-    protected $signature = 'subject_detail_cache:run';
 
-    /**
-     * The console command description.
-     *
-     * @var string
-     */
-    protected $description = '专题终端html页面静态化';
-
-    /**
-     * Create a new command instance.
-     * HotMatchCommand constructor.
-     */
-    public function __construct()
+    protected  function command_name()
     {
-        parent::__construct();
+        return "subject_detail_cache";
     }
+
+    protected function description()
+    {
+        return "专题终端html页面静态化";
+    }
+
 
     /**
      * Execute the console command.
@@ -48,25 +38,74 @@ class DetailCommand extends Command
      */
     public function handle()
     {
+        $request = new Request();
         $subCon = new SubjectController();
-        $subCon->staticSubjectLeagues(new Request());
+        $subCon->staticSubjectLeagues($request);
 
+        $type = $this->argument('type');
+        switch ($type) {
+            case "pc":
+                $this->onPcHandler($request);
+                break;
+            case "mobile":
+                $this->onMobileHandler($request);
+                break;
+            case "mip":
+                $this->onMipHandler($request);
+                break;
+            case "all":
+                $this->onPcHandler($request);
+                $this->onMobileHandler($request);
+                $this->onMipHandler($request);
+                break;
+        }
+        $this->onCommonHandler($request);
+    }
+
+    protected function onPcHandler(Request $request)
+    {
+        $leagues = SubjectLeague::getAllLeagues();
+        $subjectCon = new SubjectController();
+
+        foreach ($leagues as $league) {
+            $name_en = $league["name_en"];
+
+            $html = $subjectCon->detailV2($request, $name_en);//SubjectController::subjectDetailHtml($result, $sl);
+            if (!empty($html)) {
+                Storage::disk("public")->put("/www/$name_en/index.html", $html);
+            }
+        }
+
+    }
+
+    protected function onMobileHandler(Request $request)
+    {
         $leagues = SubjectLeague::getAllLeagues();
         $aiCon = new AikanQController();
         foreach ($leagues as $league) {
-            $this->staticSubjectHtml($aiCon, $league);
+            $result = $aiCon->subjectDetailData(false, $league);
+            if (!isset($result) || !isset($result['subject'])) {
+                return;
+            }
+            $name_en = $league["name_en"];
+
+            //手机
+            $con = new \App\Http\Controllers\Mobile\Subject\SubjectController();
+            $con->staticSubjectHtml($request, $name_en);
         }
     }
 
-
-    public function staticSubjectHtml(AikanQController $aiCon, SubjectLeague $sl) {
+    protected function staticSubjectHtml(AikanQController $aiCon, SubjectLeague $sl) {
         $result = $aiCon->subjectDetailData(false, $sl);
         if (!isset($result) || !isset($result['subject'])) {
             return;
         }
-        $html = SubjectController::subjectDetailHtml($result, $sl);
+        $subjectCon = new SubjectController();
+        $request = new Request();
+        $name_en = $sl["name_en"];
+
+        $html = $subjectCon->detailV2($request, $name_en);//SubjectController::subjectDetailHtml($result, $sl);
         if (!empty($html)) {
-            $name_en = $sl->name_en;
             Storage::disk("public")->put("/www/$name_en/index.html", $html);
         }
 
