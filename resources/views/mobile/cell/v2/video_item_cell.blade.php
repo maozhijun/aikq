8f6a99aaaab1222b2d75fae74bcd0744549d4062
ricky <843372983@qq.com>
2019-03-22 17:25:58 +0800

    aikq2 ……

--- resources/views/mobile/cell/v2/video_item_cell.blade.php
+++ resources/views/mobile/cell/v2/video_item_cell.blade.php
@@ -1,7 +1,7 @@
 @if(isset($video))
     <div class="video_item">
         <a href="{{$video['link']}}">
-            <p class="img_box" style="background-image: url({{$video['link']}});"></p>
+            <p class="img_box" style="background-image: url({{$video['image']}});"></p>
             <h3>{{$video['title']}}?</h3>
         </a>
     </div>
