445c0acf5086c44fe5cc78decdba700a8e97056b
ricky <843372983@qq.com>
2019-03-22 16:55:42 +0800

    aikq2 ……

--- resources/views/mobile/subject/v2/basketball_detail.blade.php
+++ resources/views/mobile/subject/v2/basketball_detail.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     @include('mobile.subject.v2.cell.basket.basket_info_cell', ['sl'=>$sl, 'season'=>$season, 'seasons'=>$seasons, 'hasPlayoff'=>isset($playoff)])
     @include('mobile.subject.v2.cell.basket.basket_playoff_cell', ['playoff'=>isset($playoff)?$playoff:null])
-    @include('mobile.subject.v2.cell.basket.basket_rank_cell', ['ranks'=>isset($ranks)?$ranks:null])
+    @include('mobile.subject.v2.cell.basket.basket_rank_cell', ['ranks'=>isset($ranks)?$ranks:null, 'display'=>isset($playoff)?'none':''])
     @include('mobile.subject.v2.cell.basket.basket_match_cell', ['schedules'=>isset($schedules)?$schedules:null])
     @include('mobile.subject.v2.cell.basket.basket_player_cell', ['players'=>$players])
     @include('mobile.subject.v2.cell.news_cell', ['articles'=>isset($articles)?$articles:null])
